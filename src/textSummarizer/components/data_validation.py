a6637eaf982403128c01502abf71df1f76918ada
Vishnu Vardhan Reddy Kothapuli <162018768+VishnuVardhanReddyKothapuli@users.noreply.github.com>
2026-06-05 13:01:38 +0530

    data validation notebook added

---
+++ src/textSummarizer/components/data_validation.py
@@ -0,0 +1,24 @@
+import os
+from textSummarizer.logging import logger
+from textSummarizer.entity import DataValidationConfig
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    def validate_all_files_exist(self) -> bool:
+        try:
+            all_files = os.listdir(self.config.unzip_data_dir)
+
+            validation_status = all(
+                file in all_files
+                for file in self.config.ALL_REQUIRED_FILES
+            )
+
+            with open(self.config.STATUS_FILE, "w") as f:
+                f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+
+        except Exception as e:
+            raise e
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig)
+from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -26,3 +26,15 @@ class ConfigurationManager:
 
         return create_ingestion_config
     
+    def get_data_validation_config(self) -> DataValidationConfig:
+
+        config = self.config.data_validation
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+            unzip_data_dir=config.unzip_data_dir
+        )
+
+        return data_validation_config
